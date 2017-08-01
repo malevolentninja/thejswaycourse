b11c76d60e085818894325fad80c7c45ecf4c328
Olivia Boertje <malevolentninja@users.noreply.github.com>
2017-08-01 16:16:19 +0100

    Chapter one part 1 and 2
    Completed work through of chapter one part one and two

---
+++ ChapterOne.js
@@ -0,0 +1,44 @@
+//CHAPTER ONE: 3,2,1... Code!
+
+// Statements and execution flow
+console.log("Hello from JavaScript!");
+console.log("Let's do some maths");
+console.log(4 + 7);
+console.log(12 / 0);
+console.log("Goodbye");
+
+/* multi lines
+comments */
+
+
+// About myself
+// need name and age
+age = 22;
+name = "Liv";
+console.log(name);
+console.log(age);
+
+
+//minimalistic calculator
+
+/*
+display the results of
+adding
+subtracting
+multiplication and
+dividing 6 by 3
+*/
+
+console.log(10 + 12);
+console.log(17 - 12);
+console.log(13 * 13);
+console.log( 6 / 3);
+
+// Values prediction
+/* Observe the following,
+try to predict the values it displays
+then check prediction by executing
+*/
+console.log(4 + 5); // prints 9
+console.log("4 + 5"); // prints string 4 + 5
+console.log("4" + "5"); // prints strings 45 close together
