1b385d1ca950ce6a7b5b05406aedf90ec1102d62
Olivia Boertje <malevolentninja@users.noreply.github.com>
2017-08-02 11:49:56 +0100

    Update Play_With_Variables.js
    started to add more information about variables

--- Chapter_One/Play_With_Variables.js
+++ Chapter_One/Play_With_Variables.js
@@ -1,7 +1,30 @@
 // CHAPTER ONE PART TWO: Play with Variables
 
+/*
+Variable is an information storage area. 
+Three main properties: 
+1. name - identifies it (upper and lower case, numbers(not the first part) and characters ($ or _)
+2. value - data stored in the variable
+3. type - determines the role and actions avaiable to the variable
+no need for variable type to be explicitly defined
+*/
+
+/* 
+Declaring a variable , memory reserved to store it. 
+The programme can then read and write data in this area by manipulating the variable
+*/ 
+let a;
+console.log(a);
+// returns undefined = no value
+/* 
+let followed by the variable name creates a variable. 
+var is also used like let 
+*/
+
+// Assign values to variables
+/* = assignment operator to store a value into the variable 
+this can be changed. */
 
 let a;
 a = 3.14;
 console.log(a);
-
