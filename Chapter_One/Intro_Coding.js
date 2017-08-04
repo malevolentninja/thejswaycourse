c29b84bddc8418a5167c9e59ee4d3803f2fc99c8
Olivia Boertje <malevolentninja@users.noreply.github.com>
2017-08-04 12:41:01 +0100

    Update Intro_Coding.js
    edited the answers and added comments

--- Chapter_One/Intro_Coding.js
+++ Chapter_One/Intro_Coding.js
@@ -13,8 +13,8 @@ comments */
 
 // About myself
 // need name and age
-age = 22;
-name = "Liv";
+var age = 22; 
+const name = "Liv"; // puts my name first 
 console.log(name);
 console.log(age);
 
@@ -29,10 +29,10 @@ multiplication and
 dividing 6 by 3
 */
 
-console.log(10 + 12);
-console.log(17 - 12);
-console.log(13 * 13);
-console.log( 6 / 3);
+console.log(10 + 12); // 22
+console.log(17 - 12); // 5
+console.log(13 * 13); // 169
+console.log( 6 / 3); // 2
 
 // Values prediction
 /* Observe the following,
